97e35ce634e16fb10ab2fe774aab4b7143fb8c2c
Degenal <70811870+Degenal@users.noreply.github.com>
2024-07-15 21:19:09 +0330

    Update splitSentence.py

--- splitSentence.py
+++ splitSentence.py
@@ -1,9 +1,6 @@
 def sentence_split(text):
 
-
     from IPython.display import display, HTML
-
-    
 
     import re
     sentences = re.split(r'(?<=\.)\s+', text.strip())
@@ -34,8 +31,14 @@ function copyToClipboard() {{
 
     display(HTML(button_html))
 
-    
-
 # Display the button
     return button_html
     
+
+def GetVoicesDir(VOICES_DIR):
+    VOICES = [VOICES_DIR + "sample_1.wav",
+          VOICES_DIR + "sample_2.wav",
+          VOICES_DIR + "sample_3.wav",
+          VOICES_DIR + "sample_4.wav",
+          VOICES_DIR + "sample_5.wav"
+         ]
